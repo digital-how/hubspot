54d677d6abbfaebc876dbe0e0aefb4fed4704cdc
Mert YILMAZ <digitalhow@Mac.fritz.box>
2025-10-04 23:48:14 +0200

    Create minimal HubSpot UI Extension
    - Remove React complexity and use plain JavaScript
    - Create simple HTML/CSS extension that displays basic UI
    - Minimal hsproject.json with only essential fields
    - Remove all complex dependencies and configurations
    - Use basic webpack config for simple bundling
    - This should pass HubSpot validation as it's extremely simple

--- hubspot/webpack.config.js
+++ hubspot/webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -8,44 +7,8 @@ module.exports = {
     filename: 'bundle.js',
     clean: true,
   },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  },
+  mode: 'production',
   resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html'
-    })
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'),
-    },
-    compress: true,
-    port: 3000,
-    hot: true,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
-    }
+    extensions: ['.js']
   }
 };
